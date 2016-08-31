4941a26a8cd63e5eeb5d68330a7feab03ad114f6
Per Selin <pse>
2016-08-31 11:44:11 +0200

    first commit

--- src/Organisation/Startup.cs
+++ src/Organisation/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace PTJ
+namespace Organisation
 {
     public class Startup
     {
---
+++ src/PTJ.DataLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TeleComputing AB")]
+[assembly: AssemblyProduct("PTJ.DataLayer")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("210133e1-7850-4f78-8306-8a606c6fdff8")]
