226481c139a925bdcdab99f9a0414c89fa8a060b
mahlin <marie.ahlin@ptj.se>
2016-09-15 15:44:40 +0200

    Adress methods

---
+++ AdressSvc/BusinessService/BackendCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using PTJ.DataLayer.Models;
+using PTJ.Base.BusinessRules;
+using PTJ.Base.BusinessRules.ViewModels;
+using PTJ.Message;
+using AdressSvc.BusinessService.Interfaces;
+using PTJ.Base.BusinessRules.Code;
+
+namespace AdressSvc.BusinessService
+{
+    public class BackendCode : IBackend
+    {
+        private ModelDbContext db;
+        AdressCode ac;
+        DbUtils dbUtils;
+
+        public BackendCode(ModelDbContext _db)
+        {
+            db = _db;
+            dbUtils = new DbUtils(db);
+            ac = new AdressCode(db);
+        }
+
+        public Response<PersonViewModel> GetByPersnr(long persnr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<PersonViewModel> UpdateAdress(PersonViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<PersonViewModel> InsertAdress(PersonViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<PersonViewModel> DeleteAdress(long persnr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PTJ.Base.BusinessRules/Code/AdressCode.cs
@@ -0,0 +1,20 @@
+﻿using PTJ.Base.BusinessRules.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PTJ.Base.BusinessRules.ViewModels;
+using PTJ.Message;
+using PTJ.DataLayer.Models;
+
+namespace PTJ.Base.BusinessRules.Code
+{
+    public class AdressCode : IAdress
+    {
+        private ModelDbContext db;
+        public AdressCode(ModelDbContext _db)
+        {
+            db = _db;
+        }
+    }
+}
