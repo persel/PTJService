d440f3b1756390abb1fa185a53e5517293e5b11e
taco <taco@PTJHKL2170>
2016-10-18 14:59:15 +0200

    Ändrade namn till svenska

--- src/PersonSvc/Controllers/KonsultController.cs
+++ src/PersonSvc/Controllers/KonsultController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PersonSvc.Controllers
 {
     [Route("api/[controller]")]
-    public class ConsultController : Controller
+    public class KonsultController : Controller
     {
         // GET: api/values
         [HttpGet]
